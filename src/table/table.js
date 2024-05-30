fed2ff831908b37cdcdd8907fab3120da64a1846
Anirban Aditya Halder <nabovtl@gmail.com>
2024-05-30 19:45:46 +0530

    table bug fix heading bug fix

--- src/table/table.js
+++ src/table/table.js
@@ -3,7 +3,7 @@ import './table.css'
 export default function Table(props)
 {
     return(
-        <div className="table">
+        <div className=" w-[84vw] lg:w-[78vw] grid grid-cols-2 gap-[0.625rem] text-[0.8rem] sm:text-[1.1rem] md:text-[1.325rem] leading-[1.4rem] select-none">
             <div className='intersection' >Project Type</div>
             <div className='intersection' >{props.title}</div>
 
